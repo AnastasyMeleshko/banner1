7791af53a8e0eb4b61ee18f79ab575dc1dcac479
AnastasyMeleshko <7634907@mail.ru>
2023-05-17 14:37:48 +0300

    test-02: add animation for text

--- index.js
+++ index.js
@@ -6,4 +6,15 @@ wrapper.onclick = () => {
     document.location.assign('https://google.com');
 }
 
+//text from frame2 animation
+
+const frame1 = document.getElementById('frame1');
+const frame2 = document.getElementById('frame2');
+
+setTimeout(function() {
+    frame1.style.display = 'none';
+    frame2.style.display = 'block';
+}, 8000);
+
+
 
